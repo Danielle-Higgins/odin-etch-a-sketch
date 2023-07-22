c8be9a2f51e29b380e47b246defdae05f7f2e02b
Danielle Higgins <higdan4024@gmail.com>
2023-07-22 19:37:35 -0400

    add comments

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
 const board = document.querySelector(".board")
 
-// create a 16 x 16 grid inside of our board
+// create grids inside of our board
 function creategrid(squares) {
+    // creates our row
     for (let i = 1; i <= squares; i++) {
+        // creates our column
         for (let j = 1; j <= squares; j++) {
             const grid = document.createElement("div")
+
+            // add a class to apply styling
             grid.classList.add("grid")
+
             // calculate the size of each div (500 is the size of the board)
             let size = 500 / squares
             grid.style.width = size + "px"
             grid.style.height = size + "px"
+
             board.appendChild(grid)
         }
     }
