6bd22e0986c3d261c4652d412ea7ad5aadddb232
Danielle Higgins <higdan4024@gmail.com>
2023-07-23 14:39:31 -0400

    finish buttons

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const board = document.querySelector(".board")
 const resize = document.querySelector(".resize")
+const clear = document.querySelector(".clear")
+const rainbow = document.querySelector(".rainbow")
+const black = document.querySelector(".black")
 
 // create grids inside of our board
 function creategrid(squares) {
@@ -25,7 +28,7 @@ function creategrid(squares) {
 creategrid(16)
 
 // Set up mouse drag effect so that the grid divs change color
-board.addEventListener("mouseenter", (e) => {
+board.addEventListener("mouseenter", () => {
     const grids = document.querySelectorAll(".grid")
 
     // set drag var to be false
@@ -34,7 +37,7 @@ board.addEventListener("mouseenter", (e) => {
     grids.forEach(grid => {
 
         // when we click down with our mouse, set drag to be true
-        grid.addEventListener("mousedown", (e) => drag = true)
+        grid.addEventListener("mousedown", () => drag = true)
 
         // when we move our mouse, if drag is true, set the color of each square
         grid.addEventListener("mousemove", (e) => {
@@ -44,12 +47,12 @@ board.addEventListener("mouseenter", (e) => {
         })
 
         // letting go of the mouse will set drag back to false
-        grid.addEventListener("mouseup", (e) => drag = false)
+        grid.addEventListener("mouseup", () => drag = false)
     })
 })
 
 // when user clicks on resize button
-resize.addEventListener("click", (e) => {
+resize.addEventListener("click", () => {
 
     // prompt user to enter number of squares
     let user = prompt("How many squares per side? (Max is 100)")
@@ -78,4 +81,72 @@ resize.addEventListener("click", (e) => {
 
     // creating a new grid based on user input
     creategrid(user)
+})
+
+// when the user clicks on the clear button
+clear.addEventListener("click", () => {
+    const grids = document.querySelectorAll(".grid")
+
+    // remove the background color of each square
+    grids.forEach(grid => {
+        grid.style.backgroundColor = ""
+    })
+})
+
+// change the background color to random rgb
+rainbow.addEventListener("click", () => {
+
+    board.addEventListener("mouseenter", () => {
+        const grids = document.querySelectorAll(".grid")
+    
+        // set drag var to be false
+        let drag = false
+    
+        grids.forEach(grid => {
+    
+            // when we click down with our mouse, set drag to be true
+            grid.addEventListener("mousedown", () => drag = true)
+    
+            // when we move our mouse, if drag is true, set the color of each square
+            grid.addEventListener("mousemove", (e) => {
+                if (drag === true) {
+                    // each time the mouse moves, change rgb value
+                    let red = Math.floor(Math.random() * 256)
+                    let green = Math.floor(Math.random() * 256)
+                    let blue = Math.floor(Math.random() * 256)
+
+                    e.target.style.backgroundColor = "rgb(" + red + "," + green + "," + blue + ")"
+                }
+            })
+    
+            // letting go of the mouse will set drag back to false
+            grid.addEventListener("mouseup", () => drag = false)
+        })
+    })
+})
+
+// revert back to original color
+black.addEventListener("click", () => {
+    board.addEventListener("mouseenter", () => {
+        const grids = document.querySelectorAll(".grid")
+    
+        // set drag var to be false
+        let drag = false
+    
+        grids.forEach(grid => {
+    
+            // when we click down with our mouse, set drag to be true
+            grid.addEventListener("mousedown", () => drag = true)
+    
+            // when we move our mouse, if drag is true, set the color of each square
+            grid.addEventListener("mousemove", (e) => {
+                if (drag === true) {
+                    e.target.style.backgroundColor = "black"
+                }
+            })
+    
+            // letting go of the mouse will set drag back to false
+            grid.addEventListener("mouseup", () => drag = false)
+        })
+    })
 })
